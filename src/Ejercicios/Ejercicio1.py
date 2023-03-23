4f6eaa81e46a59d361907718fa8274d5eb71b5b9
NiCo7889 <113980882+NiCo7889@users.noreply.github.com>
2023-03-23 18:22:18 +0100

    cambios

--- src/Ejercicios/Ejercicio1.py
+++ src/Ejercicios/Ejercicio1.py
@@ -5,3 +5,99 @@ de pasarlos todos desde la primera aguja a la tercera, con dos condiciones, solo
 Se dijo a los sacerdotes que, cuando hubieran terminado de mover los discos, llegaría el fin del mundo. Resolver este problema de la Torre de Hanói.
 """
 # hay que hacer uso de las colas para resolverlo
+
+
+class Pila:
+    def __init__(self):
+        self.items = []
+
+    def esta_vacia(self):
+        return len(self.items) == 0
+
+    def apilar(self, item):
+        self.items.append(item)
+
+    def desapilar(self):
+        if self.esta_vacia():
+            return None
+        return self.items.pop()
+
+
+def torre_hanoi_pila(n):
+    A = Pila()
+    B = Pila()
+    C = Pila()
+
+    # Llenar pila A con los n discos
+    for i in range(n, 0, -1):
+        A.apilar(i)
+
+    if n % 2 == 0:
+        while not C.esta_vacia() or not B.esta_vacia():
+            if not C.esta_vacia() and (B.esta_vacia() or C.items[-1] < B.items[-1]):
+                mover_disco(C, B)
+            else:
+                mover_disco(B, C)
+    else:
+        while not C.esta_vacia() or not A.esta_vacia():
+            if not C.esta_vacia() and (A.esta_vacia() or C.items[-1] < A.items[-1]):
+                mover_disco(C, A)
+            else:
+                mover_disco(A, C)
+
+
+def mover_disco(desde, hacia):
+    disco = desde.desapilar()
+    hacia.apilar(disco)
+    print(f"Mover disco de {desde} a {hacia}")
+
+print(torre_hanoi_pila(4))
+
+
+# class Pila:
+#     def __init__(self):
+#         self.items = []
+
+#     def esta_vacia(self):
+#         return self.items == []
+
+#     def agregar(self, item):
+#         self.items.append(item)
+
+#     def quitar(self):
+#         return self.items.pop()
+
+# def torre_hanoi_pila(n):
+#     pila_origen = Pila()
+#     pila_auxiliar = Pila()
+#     pila_destino = Pila()
+
+#     # Llenar la pila de origen con los discos
+#     for i in range(n, 0, -1):
+#         pila_origen.agregar(i)
+
+#     # Definir la dirección de los movimientos
+#     if n % 2 == 0:
+#         direccion = ["A", "C", "B"]
+#     else:
+#         direccion = ["A", "B", "C"]
+
+#     # Realizar los movimientos
+#     movimiento = 0
+#     while len(pila_destino.items) != n:
+#         movimiento += 1
+#         if movimiento % 3 == 1:
+#             mover_disco(pila_origen, pila_destino, direccion[0], direccion[1])
+#         elif movimiento % 3 == 2:
+#             mover_disco(pila_origen, pila_auxiliar, direccion[0], direccion[2])
+#         else:
+#             mover_disco(pila_auxiliar, pila_destino, direccion[2], direccion[1])
+
+# def mover_disco(pila_origen, pila_destino, origen, destino):
+#     if not pila_origen.esta_vacia() and (pila_destino.esta_vacia() or pila_origen.items[-1] < pila_destino.items[-1]):
+#         disco = pila_origen.quitar()
+#         pila_destino.agregar(disco)
+#         print(f"Mover disco de {origen} a {destino}")
+
+# # Ejemplo de uso para el problema planteado en el ejercicio:
+# print(torre_hanoi_pila(2))
--- src/Ejercicios/Ejercicio2.py
+++ src/Ejercicios/Ejercicio2.py
@@ -2,36 +2,88 @@
 Realiza el  código para calcular el determinante de una matriz cuadrada de [3 x 3], regla de Sarrus de forma recursiva y de forma iterativa.
 """
 
+# FORMA RECURSIVA
 
-# Forma recursiva
+import copy
+
+def determinante_manera_recursiva(Matriz, total=0):
+    """
+    Función para el cálculo recursivo del determinante de cualquier matriz cuadrada.
+    """
+    tamaño = list(range(len(Matriz))) # tamaño que tendrá la matriz
+
+    if len(Matriz) == 2 and len(Matriz[0]) == 2: # Si la matriz es de 2x2
+        val = Matriz[0][0] * Matriz[1][1] - Matriz[1][0] * Matriz[0][1] # Calculamos el determinante
+        return val # Devolvemos el determinante
+
+    for fc in tamaño: # Para cada columna de la matriz ...
+        As = copy.deepcopy(Matriz)  # haz una copia, y ...
+        As = As[1:] # ... elimina la primera fila
+        height = len(As) # Altura de la matriz
+
+        for i in range(height):  # Para cada fila de la submatriz ...
+            As[i] = As[i][0:fc] + As[i][fc+1:] # elimina la columna de la matriz
+
+        sign = (-1) ** (fc % 2)  # Alternar signos para el multiplicador de la submatriz
+        sub_det = determinante_manera_recursiva(As)  # Pasar submatriz recursivamente
+        total += sign * Matriz[0][fc] * sub_det # Total de todos los retornos de la recursión
+
+    return total # Devolvemos el determinante
+
+# Creamos una matriz cuadrada de tamaño nxn
+n = 3
+Matriz = [[0] * n for i in range(n)]
+
+# Rellenamos la matriz con valores aleatorios
+import random
+
+for i in range(n):
+    for j in range(n):
+        Matriz[i][j] = random.randint(-10, 10)
+
+# Imprimimos la matriz
+print("Matriz: ")
+for i in range(n):
+    for j in range(n):
+        print(Matriz[i][j], end=" ")
+    print()
+
+# Si quiero calcular una matriz concreta
+# Matriz = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] 
+
+# Calculamos el determinante de la matriz
+determinante = determinante_manera_recursiva(Matriz)
+print("Resultado del determinante de la matriz ramdom calculado de manera recursiva: ", determinante)
 
 
 
 
-
-# Forma iterativa
-M1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] # Definir una matriz cuadrada de tamaño 3x3
-m = len(M1)# Definir m como el tamaño de la matriz M1
+# FORMA ITERATIVA
+Matriz1 = [[1, 2, 3],
+           [4, 5, 6],
+           [7, 8, 9]]
+size = len(Matriz1) # El tamaño de la Matriz1
+print("Matriz1: ", Matriz1)
 
 aux = 0 # aux es la variable que almacena el resultado del determinante
-for o in range(0, m): # o es la variable que recorre las filas de la matriz
+for o in range(0, size): # o es la variable que recorre las filas de la matriz
     temp = 1 # temp es la variable que almacena el resultado de la multiplicación de los elementos de la fila
     k = o # k es la variable que recorre las columnas de la matriz
-    for i in range(0, m): # i es la variable que recorre las filas de la matriz
-        temp *= M1[i][k] # Multiplicar los elementos de la fila
+    for i in range(0, size): # i es la variable que recorre las filas de la matriz
+        temp *= Matriz1[i][k] # Multiplicar los elementos de la fila
         k += 1  # Incrementar k en 1
-        if k == m: # Si k es igual al tamaño de la matriz
+        if k == size: # Si k es igual al tamaño de la matriz
             k = 0 # k es igual a 0
     aux += temp # Sumar el resultado de la multiplicación de los elementos de la fila
 
-    for o in range(m-1, -1, -1): # o es la variable que recorre las filas de la matriz
-        temp = 1 # temp es la variable que almacena el resultado de la multiplicación de los elementos de la fila
-        k = o # k es la variable que recorre las columnas de la matriz
-        for i in range(0, m): # i es la variable que recorre las filas de la matriz
-            temp *= M1[i][k] # Multiplicar los elementos de la fila
-            k -= 1 # Decrementar k en 1
-            if k == -1: # Si k es igual a -1
-                k = m - 1 # k es igual al tamaño de la matriz - 1
-        aux -= temp # Restar el resultado de la multiplicación de los elementos de la fila
+for o in range(size-1, -1, -1): # o es la variable que recorre las filas de la matriz
+    temp = 1 # temp es la variable que almacena el resultado de la multiplicación de los elementos de la fila
+    k = o # k es la variable que recorre las columnas de la matriz
+    for i in range(0, size): # i es la variable que recorre las filas de la matriz
+        temp *= Matriz1[i][k] # Multiplicar los elementos de la fila
+        k -= 1 # Decrementar k en 1
+        if k == -1: # Si k es igual a -1
+            k = size - 1 # k es igual al tamaño de la matriz - 1
+    aux -= temp # Restar el resultado de la multiplicación de los elementos de la fila
 
-print("El determinante de la matriz calculada de manera iterativa es: ", aux)
+print("El determinante de la matriz1 calculado de manera iterativa es: ", aux)
